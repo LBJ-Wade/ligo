3dcc8ac68e93d7b50e7f68862134a88811c46963
Qiwen Wang <qwang70@illinois.edu>
2016-06-30 17:41:45 -0500

    upload yw.properties and run.sh

--- csv/run.sh
+++ csv/run.sh
@@ -4,10 +4,8 @@ FILE=*.csv
 
 for f in $FILE
 do
-
  NAME=`echo "$f" | cut -d'.' -f1`
  MESSAGE=".mode csv\n.import $f $NAME"
- echo "Processing $MESSAGE"
  echo -e $MESSAGE | sqlite3 model_query.db
  # do something on $f
 done
